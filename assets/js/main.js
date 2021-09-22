07b55ae177bc1922a3eefd79e20d28853b47f174
quannh2807 <helgrindxxx@gmail.com>
2021-09-22 11:42:16 +0700

    UI: Responsive Navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-// $('.collapse').collapse()
+let menu = new MmenuLight(document.querySelector('#menu'), 'all');
+
+let navigator = menu.navigation({
+	// selectedClass: 'Selected',
+	// slidingSubmenus: true,
+	// theme: 'dark',
+	// title: 'Menu'
+});
+
+let drawer = menu.offcanvas({
+	position: 'right',
+});
+
+//	Open the menu.
+document.querySelector('button.btn-expand').addEventListener('click', (event) => {
+	event.preventDefault();
+	drawer.open();
+});
