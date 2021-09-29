374ef400c0fc013425544e31ffa20ff95ba72c37
quannh2807 <helgrindxxx@gmail.com>
2021-09-29 22:53:44 +0700

    Update collapse footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-let menu = new MmenuLight(document.querySelector('#menu'), 'all');
+/* let menu = new MmenuLight(document.querySelector('#menu'), 'all');
 
 let navigator = menu.navigation({
 	// selectedClass: 'Selected',
@@ -15,4 +15,32 @@ let drawer = menu.offcanvas({
 document.querySelector('button.btn-expand').addEventListener('click', (event) => {
 	event.preventDefault();
 	drawer.open();
-});
+}); */
+
+window.onresize = function () {
+	let windowWidth = window.innerWidth;
+	document.querySelectorAll('.btn-collapse').forEach((element) => {
+		let attr = element.getAttribute('data-bs-toggle');
+		if (windowWidth > 768 && attr.length > 0) {
+			element.setAttribute('data-bs-toggle', '');
+			let listActiveE = document.querySelectorAll('.accordion-collapse');
+			listActiveE.forEach((e) => e.classList.add('show'));
+		} else if (windowWidth < 768 && !attr.length > 0) {
+			element.setAttribute('data-bs-toggle', 'collapse');
+
+			let listActiveE = document.querySelectorAll('.accordion-collapse');
+			listActiveE.forEach((e) => e.classList.remove('show'));
+		}
+	});
+};
+window.onload = function () {
+	let listActiveE = document.querySelectorAll('.accordion-collapse');
+	listActiveE.forEach((e) => {
+		let isContain = e.classList.contains('show');
+		if (!isContain && window.innerWidth > 767) {
+			e.classList.add('show');
+		} else if (isContain && window.innerWidth < 768) {
+			e.classList.remove('show');
+		}
+	});
+};
